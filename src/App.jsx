53a41900b33f265bcff026d74f66380a1e5ace68
sabbir053 <sabbirhossain0900@gmail.com>
2026-03-18 00:11:48 +0600

    React World Countries Visit Project Start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Countries from './components/countries/Countries'
+
+function App() {
+
+  return (
+    <>
+    <Countries></Countries>
+      <h1>Start a new Project....</h1>
+    </>
+  )
+}
+
+export default App
